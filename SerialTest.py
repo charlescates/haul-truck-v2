231f51ca616807bc6f589c56d69c9e4c38d87f7d
charlescates <149528073+charlescates@users.noreply.github.com>
2024-04-16 14:49:18 -0400

    Update SerialTest.py
    Added timeout as 1s

--- SerialTest.py
+++ SerialTest.py
@@ -2,6 +2,7 @@ import serial
 from time import sleep
 #'/dev/serial0' might be the wrong port
 serialPort = serial.Serial('/dev/serial0')
+serialPort.timeout = 1
 print(serialPort.name)
 serialPort.write(b'test\n')
 sleep(1)
