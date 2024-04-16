18b9a17a75a6227bb7b7e70ce00be6a03f90df66
charlescates <149528073+charlescates@users.noreply.github.com>
2024-04-16 14:32:26 -0400

    Update SerialTest.py

--- SerialTest.py
+++ SerialTest.py
@@ -2,3 +2,6 @@ import pyserial
 #'/dev/serial0' might be the wrong port
 serialPort = serial.Serial('/dev/serial0')
 print(serialPort.name)
+serialPort.write("test\n")
+thing = serialPort.readline()
+print(thing)
